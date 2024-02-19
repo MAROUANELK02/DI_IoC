c7934a7e25e0b71500b83a1e9e048950df8c8f4c
Marouane LASMAK <116379216+MAROUANELK02@users.noreply.github.com>
2024-02-19 09:34:40 +0100

    first-commit

---
+++ src/main/java/metier/IMetierImpl.java
@@ -0,0 +1,28 @@
+package metier;
+
+import dao.IDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IMetierImpl implements IMetier {
+    @Autowired
+    @Qualifier("dao2")
+    private IDao dao;
+
+    /*public IMetierImpl(IDao dao) {
+        this.dao = dao;
+    }*/
+
+    @Override
+    public double calcul() {
+        double tmp = dao.getData();
+        double res = tmp*540/Math.cos(tmp*Math.PI);
+        return res;
+    }
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
---
+++ src/main/java/presentation/Pres1.java
@@ -0,0 +1,16 @@
+package presentation;
+
+import dao.IDaoImpl;
+import ext.IDaoImpl2;
+import metier.IMetierImpl;
+
+public class Pres1 {
+    public static void main(String[] args) {
+        IDaoImpl dao = new IDaoImpl();
+        IDaoImpl2 dao2 = new IDaoImpl2();
+        //IMetierImpl metier = new IMetierImpl(dao2);
+
+        //System.out.println("Résultat : "+metier.calcul());
+
+    }
+}
